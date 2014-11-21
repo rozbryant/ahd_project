db949ddf1f958c88388803a07a760376acbb9702
Roz Bryant <rozbryant@gmail.com>
2014-11-21 12:06:22 -0600

    sTR

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,2 +1,5 @@
 class Building < ActiveRecord::Base
+  has_many(:amenities, { :class_name => "Amenity", :foreign_key => "building_id"})
+
+  has_many(:addresses, { :class_name => "Address", :foreign_key => "building_id"})
 end
